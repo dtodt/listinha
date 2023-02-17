cc121b758fb48a8b07b00373f51117c03a547711
Daniel Otavio Todt <danieltodt@gmail.com>
2023-02-17 08:17:53 -0300

    chore: lesson 7 - configuration page

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -10,9 +10,75 @@ class ConfigurationPage extends StatefulWidget {
 class _ConfigurationPageState extends State<ConfigurationPage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final title = textTheme.titleLarge?.copyWith(
+      fontWeight: FontWeight.bold,
+    );
+    final subTitle = textTheme.titleMedium?.copyWith(
+      fontWeight: FontWeight.bold,
+    );
+    final option = textTheme.titleSmall;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('LISTINHA'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Configurações',
+              style: title,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Tema',
+              style: subTitle,
+            ),
+            const SizedBox(height: 20),
+            RadioListTile<ThemeMode>(
+              groupValue: ThemeMode.system,
+              value: ThemeMode.light,
+              title: Text(
+                'Claro',
+                style: option,
+              ),
+              onChanged: (value) {},
+            ),
+            RadioListTile<ThemeMode>(
+              groupValue: ThemeMode.system,
+              value: ThemeMode.dark,
+              title: Text(
+                'Escuro',
+                style: option,
+              ),
+              onChanged: (value) {},
+            ),
+            RadioListTile<ThemeMode>(
+              groupValue: ThemeMode.system,
+              value: ThemeMode.system,
+              title: Text(
+                'Sistema',
+                style: option,
+              ),
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Controle de dados',
+              style: subTitle,
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('Apagar cache e reiniciar o app'),
+            ),
+          ],
+        ),
       ),
     );
   }
