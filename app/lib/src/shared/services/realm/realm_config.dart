fcb00066fff605b9e344af09966ce1b6f051d3c2
Daniel Otavio Todt <danieltodt@gmail.com>
2023-02-17 10:10:26 -0300

    chore: lesson 9 - task model

--- app/lib/src/shared/services/realm/realm_config.dart
+++ app/lib/src/shared/services/realm/realm_config.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
 
 import 'models/configuration_model.dart';
+import 'models/task_model.dart';
 
 LocalConfiguration get config => Configuration.local(
       [
         ConfigurationModel.schema,
+        Task.schema,
+        TaskBoard.schema,
       ],
       initialDataCallback: (realm) {
         realm.add(ConfigurationModel(ThemeMode.system.name));
